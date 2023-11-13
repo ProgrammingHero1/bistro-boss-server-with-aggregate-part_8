c92893a9d2df4df289161e9c703122f127fdfaef
Programming Hero <programming.hero1@gmail.com>
2023-11-12 19:39:35 -0600

    added carts get api for specific user

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ async function run() {
     })
 
     // carts collection
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
