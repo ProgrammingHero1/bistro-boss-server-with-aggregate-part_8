3ec62e7634ef2a291ee4b44b6f9712e4972adb8f
Programming Hero <programming.hero1@gmail.com>
2023-11-13 23:16:39 -0600

    install jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
