467d977f6c49c8c2c103ac97ba44e419aaa3d81d
Programming Hero <programming.hero1@gmail.com>
2023-11-14 20:21:24 -0600

    check admin api

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ async function run() {
         return res.status(401).send({ message: 'forbidden access' });
       }
       const token = req.headers.authorization.split(' ')[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-        if(err){
-          return res.status(401).send({message: 'forbidden access'})
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' })
         }
         req.decoded = decoded;
         next();
@@ -60,6 +60,21 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'unauthorized access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
 
     app.post('/users', async (req, res) => {
       const user = req.body;
