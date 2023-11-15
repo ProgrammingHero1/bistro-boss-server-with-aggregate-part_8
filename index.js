e4be243f193f4816675882537793420707ca4116
Programming Hero <programming.hero1@gmail.com>
2023-11-14 19:28:19 -0600

    verify jwt token

--- index.js
+++ index.js
@@ -41,13 +41,18 @@ async function run() {
 
     // middlewares 
     const verifyToken = (req, res, next) => {
-      console.log('inside verify token', req.headers);
+      console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: 'forbidden access' });
       }
       const token = req.headers.authorization.split(' ')[1];
-      
-      // next();
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
     }
 
     // users related api
