151039cd4024f8858b68e3d629b1c012b40bcf8c
Programming Hero <programming.hero1@gmail.com>
2023-11-15 21:47:18 -0600

    Delete menu item with VerifyAdmin middleware

--- index.js
+++ index.js
@@ -131,6 +131,13 @@ async function run() {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
       res.send(result);
+    });
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
     })
 
     app.get('/reviews', async (req, res) => {
