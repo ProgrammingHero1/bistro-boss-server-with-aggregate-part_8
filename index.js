8cf329da36e7429f916b5ad8d99f8e73a6d62625
Programming Hero <programming.hero1@gmail.com>
2023-11-19 14:24:31 -0600

    created the admin stats api

--- index.js
+++ index.js
@@ -238,6 +238,31 @@ async function run() {
       res.send({ paymentResult, deleteResult });
     })
 
+    // stats or analytics
+    app.get('/admin-stats', async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // this is not the best way
+      // const payments = await paymentCollection.find().toArray();
+      // const revenue = payments.reduce((total, payment) => total + payment.price, 0);
+
+      const result = await paymentCollection.aggregate([
+        {
+          
+        }
+      ])
+
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue
+      })
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
