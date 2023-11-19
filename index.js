9189af987001fea039ebf2cdd5ef13ec301860b9
Programming Hero <programming.hero1@gmail.com>
2023-11-18 22:36:47 -0600

    get user specific payment history

--- index.js
+++ index.js
@@ -209,21 +209,33 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret
       })
+    });
+
+
+    app.get('/payments/:email', verifyToken, async (req, res) => {
+      const query = { email: req.params.email }
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
     })
 
-    app.post('/payments', async(req, res) =>{
+    app.post('/payments', async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
 
       //  carefully delete each item from the cart
       console.log('payment info', payment);
-      const query = {_id: {
-        $in: payment.cartIds.map(id => new ObjectId(id))
-      }};
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      };
 
       const deleteResult = await cartCollection.deleteMany(query);
 
-      res.send({paymentResult, deleteResult});
+      res.send({ paymentResult, deleteResult });
     })
 
     // Send a ping to confirm a successful connection
